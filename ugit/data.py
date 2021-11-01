37c08b76d70e092e2952432af98198703b6014fe
stay7 <codelatte20@gmail.com>
2021-11-01 20:58:46 +0900

    Add types to objects

--- ugit/data.py
+++ ugit/data.py
@@ -9,13 +9,24 @@ def init():
     os.makedirs(f"{GIT_DIR}/objects")
 
 
-def hash_object(data):
+# 저장할 때 file type을 앞에 붙인다. default 는 blob
+# oid는 file data를 hash한 값
+def hash_object(data, type_="blob"):
+    obj = type_.encode() + b"\x00" + data
     oid = hashlib.sha1(data).hexdigest()
     with open(f"{GIT_DIR}/objects/{oid}", "wb") as out:
-        out.write(data)
+        out.write(obj)
     return oid
 
 
-def get_object(oid):
+# oid를 이용해 file을 찾아서 저장
+def get_object(oid, expected="blob"):
     with open(f"{GIT_DIR}/objects/{oid}", "rb") as f:
-        return f.read()
+        obj = f.read()
+
+    type_, _, content = obj.partition(b"\x00")
+    type_ = type_.decode()
+
+    if expected is not None:
+        assert type_ == expected, f"Expected {expected}, got {type_}"
+    return content
